5f7c98f6de9f244ce1f3acd09ebd83769d1823ac
FLOREA GEORGE <george.florea@essensys.ro>
2024-04-19 18:44:10 +0300

    .

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 export default {
-  contractAddress: "0x6b812f4D68fC396b6e61dD2a5Ef8A743a9122180",
-  reservationAddress: "0xfFc9a4D349364031EFB4e47b9AFa6f0cA79FB534",
+  contractAddress: "0x8C3b0468c8197Fa9b7069a027d8578e91261f4da",
+  reservationAddress: "0x6626a22Ef3137D2553128e2b679a99d7d23e689B",
 };
